7d4a8ff3bc058f47137e37aef0c5bbe1cfe9f06b
Robert Scott <keyboard.operator@gmail.com>
2019-04-03 12:36:55 -0700

    Simplify App.js further.

--- App.js
+++ App.js
@@ -7,15 +7,8 @@
  */
 
 import React, { Component } from "react"
-import { Platform, StyleSheet, Text, View } from "react-native"
-import { WebView, WebViewMessageEvent } from "react-native-webview"
-
-const instructions = Platform.select({
-	ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-	android:
-		"Double tap R on your keyboard to reload,\n" +
-		"Shake or press menu button for dev menu",
-})
+import { StyleSheet, View } from "react-native"
+import { WebView } from "react-native-webview"
 
 type Props = {}
 export default class App extends Component<Props> {
@@ -31,11 +24,6 @@ export default class App extends Component<Props> {
 			</View>
 		)
 	}
-
-	handleMessage = event => {}
-
-	handleFinishLoading = () => {}
-	handleStartLoading = () => {}
 }
 
 const styles = StyleSheet.create({
@@ -45,16 +33,6 @@ const styles = StyleSheet.create({
 		justifyContent: "center",
 		alignItems: "center",
 		backgroundColor: "#F5FCFF",
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: "center",
-		margin: 10,
-	},
-	instructions: {
-		textAlign: "center",
-		color: "#333333",
-		marginBottom: 5,
 	},
 	webview: {
 		flex: 1,
